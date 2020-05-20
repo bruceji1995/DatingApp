fa5f5b76ea02936c486f004ab4278facb0d62062
bruceji1995 <bruce.ji.1995@gmail.com>
2020-05-20 17:23:46 -0400

    Generated Users entity and Registor and Login Auth

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using DatingApp.API.Daata;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+        {
+            //// validate request if don't use api controller 
+            // if(!ModelState.IsValid)
+            //     return BadRequest(ModelState);
+
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.UserName))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+            
+        }
+
+
+    
+    }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,9 +28,11 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
